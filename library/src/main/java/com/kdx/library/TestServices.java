6729bc149c69ae80a1d2bd1ef514f91b49d4a1e5
zhangpengfei <zhangpengfei@kdxfilm.com>
2017-07-20 11:59:57 +0800

    keyevent 有輸出

--- library/src/main/java/com/kdx/library/TestServices.java
+++ library/src/main/java/com/kdx/library/TestServices.java
@@ -7,37 +7,43 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.util.Log;
+import android.view.KeyEvent;
 
 import com.kdx.library.utils.LockScreenUtils;
+import com.kdx.library.utils.PowerUtils2;
 
 /**
  * Created by kdx on 2017/7/19.
  * Email:zhangpengfei@kdxfilm.com
-*/
+ */
 
 public class TestServices extends Service {
     @Override
     public IBinder onBind(Intent intent) {
-        Log.i("999","  ==  TestServices  onBind  ==  ");
+        Log.i("999", "  ==  TestServices  onBind  ==  ");
         return null;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i("999","  ==  TestServices  onCreate  ==  ");
-        LockScreenUtils.getWakeLock(this).acquire(20 * 1000);        //唤醒
-//        LockScreenUtils.getWakeLock(this).acquire();        //唤醒
-////        acquireWakeLock();
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                LockScreenUtils.getWakeLock(TestServices.this).setReferenceCounted(false);
-//                LockScreenUtils.getWakeLock(TestServices.this).release();
-//                Log.i("999","  ==  TestServices  release  ==  ");
-////                releaseWakeLock();
-//            }
-//        },5 * 1000);
+        Log.i("999", "  ==  TestServices  onCreate  ==  ");
+//        LockScreenUtils.getWakeLock(this).acquire(20 * 1000);        //唤醒
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("999", "  ==  TestServices  Runnable   run  ==  ");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PowerUtils2.sendKeyCode(KeyEvent.KEYCODE_POWER);
+                    }
+                }).start();
+            }
+        }, 5 * 1000);
+
+//        Log.i("999", " = 输入指令 " + args[1] + " = " + keyCode + "");
 
     }
 }
---
+++ library/src/main/java/com/kdx/library/utils/PowerUtils2.java
@@ -0,0 +1,66 @@
+package com.kdx.library.utils;
+
+import android.app.Instrumentation;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by kdx on 2017/7/20.
+ * Email:zhangpengfei@kdxfilm.com
+ */
+
+public class PowerUtils2 {
+
+    public static void sendKeyCode(int keyCode) {
+//        try {
+//            Instrumentation inst = new Instrumentation();
+//            inst.sendKeyDownUpSync( keyCode );
+//        } catch (Exception e) {
+//            Log.e("999", "  = 在执行 input keycode 时出错  == "+e.toString());
+//        }
+        String[] args = {"input", "keyevnet", keyCode + ""};
+            String result = "";
+            ProcessBuilder processBuilder = new ProcessBuilder(args);
+            Process process = null;
+            InputStream errIs = null;
+            InputStream inIs = null;
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                int read = -1;
+                process = processBuilder.start();
+                errIs = process.getErrorStream();
+                while ((read = errIs.read()) != -1) {
+                    baos.write(read);
+                }
+                baos.write('/'+'n');
+                inIs = process.getInputStream();
+                while ((read = inIs.read()) != -1) {
+                    baos.write(read);
+                }
+                byte[] data = baos.toByteArray();
+                result = new String(data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (errIs != null) {
+                        errIs.close();
+                    }
+                    if (inIs != null) {
+                        inIs.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (process != null) {
+                    process.destroy();
+                }
+            }
+            Log.i("999"," = 执行 input keyevnet 后的输出结果 = "+ result);
+    }
+}
